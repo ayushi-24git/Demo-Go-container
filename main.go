d46a994f8f4c2cb8925d932b7ddf9f11e40e5257
ayushi-24git <rastogee.ayushi@gmail.com>
2021-02-09 20:09:39 +0530

    Pushed Go api

--- main.go
+++ main.go
@@ -2,21 +2,19 @@ package main
 
 import (
     "fmt"
-    "html"
+    
     "log"
     "net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World!")
+}
+
 func main() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
-    })
-
+    http.HandleFunc("/", home)
+    
     log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
